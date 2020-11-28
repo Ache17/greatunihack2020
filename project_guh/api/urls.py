bc1f01478f6817c105037d7976f1985a48739a19
Mark Turner <14060149+MarkTurner01@users.noreply.github.com>
2020-11-28 17:55:09 +0000

    Added "read from cache function" for Xnewscache.json

--- project_guh/api/urls.py
+++ project_guh/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import FireView, EarthView, NewsViewDirect
+from .views import FireView, EarthView, NewsViewDirect, NewsView
 
 
 urlpatterns = [
         path('fire',FireView.as_view(), name='test'),
         path('earth',EarthView.as_view(), name='test'),
-        path('news/<str:query>',NewsViewDirect.as_view(), name='test')
+        path('news/<str:query>',NewsViewDirect.as_view(), name='test'),
+        path('newscached/<str:query>',NewsView.as_view(), name="test")
 ]
