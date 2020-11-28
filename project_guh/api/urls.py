0a5d5119fcec9cd802f1800f0856b485fe015a41
Mark Turner <14060149+MarkTurner01@users.noreply.github.com>
2020-11-28 15:45:51 +0000

    Get News added (directly from API)
    Routing URL: '/news/wildfire' to get news with 'wildfire' in the title. 20 results at the minute, but this can be edited if we need more / less

--- project_guh/api/urls.py
+++ project_guh/api/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
-from .views import FireView, EarthView
+from django.urls import path, include
+from .views import FireView, EarthView, NewsViewDirect
 
 
 urlpatterns = [
         path('fire',FireView.as_view(), name='test'),
         path('earth',EarthView.as_view(), name='test'),
+        path('news/<str:query>',NewsViewDirect.as_view(), name='test')
 ]
