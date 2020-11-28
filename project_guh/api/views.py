30d50dc91a558916f22b1c105dcdeee98060fe5d
Maciej <maciej.lewandowski@student.manchester.ac.uk>
2020-11-28 17:26:12 +0000

    modified API and added earthquakes visualization

--- project_guh/api/views.py
+++ project_guh/api/views.py
@@ -5,51 +5,50 @@ from rest_framework.views import APIView
 from .models import Earthquakes
 import requests
 import os
+import json
+
+current_path = os.path.dirname(__file__)
+fire_pth = os.path.join(current_path, 'fire_data_europe.geojson')
+try:
+    with open(fire_pth) as f:
+        fire_content = f.read()
+except Exception as e:
+    fire_content = "{'message' : 'not found'}"
+fire_json = json.loads(fire_content)
+
+earthquakes_pth = os.path.join(current_path, "earth_data.geojson")
+try:
+    with open(earthquakes_pth) as f:
+        earthquakes_content = f.read()
+except Exception as e:
+    earthquakes_content = "{'message' : 'not found'}"
+earthquakes_json = json.loads(earthquakes_content)
 
 
 class FireView(APIView):
     def get(self, request):
-    	content = {"Hello" : "world"}
-    	current_path = os.path.dirname(__file__)
-    	fire_data = os.path.join(current_path, 'fire_data_europe.geojson')
-    	if request.method == 'GET':
-    		try:
-    			f = open(fire_data)
-    			json_string = f.read()
-    			f.close()
-    			data = json.loads(json_string)
-    			response = json.dumps(data)
-    		except:
-    			response = json.dumps([{'Error': 'No fires found.'}])
-    	return Response(response, content_type='text/json')
+        return Response(fire_json, content_type='text/json')
 
     def post(self, request):
-        content = {"POST " :"This is a message"}
+        content = {"POST ": "This is a message"}
         return Response(content)
 
+
 class EarthView(APIView):
-	def get(self, request):
-		current_path = os.path.dirname(__file__)
-		fire_data = os.path.join(current_path, 'earth_data.geojson')
-		if request.method == 'GET':
-			try:
-				f = open(fire_data)
-				json_string = f.read()
-				f.close()
-				data = json.loads(json_string)
-				response = json.dumps(data)
-			except:
-				response = json.dumps([{'Error': 'No earthquakes found.'}])
-		return Response(response, content_type='text/json')
+    def get(self, request):
+        return Response(earthquakes_json, content_type='text/json')
+
 
 class NewsViewDirect(APIView):
-	def get(self, request, query="wildfires"):
-		if request.method == "GET":
-			payload = {"qInTitle":query,"apiKey":"487da61fbf4a4542bd67e7bcad0e3f82"}
-			response = requests.get("https://newsapi.org/v2/everything",payload)
-			if response.status_code == 200:
-				with  open(os.path.join(os.path.dirname(__file__)+query+'newscache.json'),"w") as outfile:
-					json.dump(response.json(),outfile)
-				return Response(response.json())
-			else: return Response({"Error":"Request failed"})
-		else: return Reponse({"Error":"Method not allowed."})
+    def get(self, request, query="wildfires"):
+        if request.method == "GET":
+            payload = {"qInTitle": query, "apiKey": "487da61fbf4a4542bd67e7bcad0e3f82"}
+            response = requests.get("https://newsapi.org/v2/everything", payload)
+            if response.status_code == 200:
+                with  open(os.path.join(os.path.dirname(__file__) + query + 'newscache.json'), "w") as outfile:
+                    json.dump(response.json(), outfile)
+                return Response(response.json())
+            else:
+                return Response({"Error": "Request failed"})
+        else:
+            return Response({"Error": "Method not allowed."})
--- project_guh/frontend/static/js/main.js
+++ project_guh/frontend/static/js/main.js
@@ -2,89 +2,250 @@
 var map = null;
 var mapCanvas = null;
 
-function addMap(id)
-{
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYWNoZSIsImEiOiJja2hzMzNiZTAwZzhzMnFsaGRrb2RiazI1In0.Kas8OyDCtoysgjokizoasg';
-    map = new mapboxgl.Map({
-        container : "map",
-        //style: 'mapbox://styles/mapbox/satellite-v9', // stylesheet location
-        style : "mapbox://styles/mapbox/dark-v10",
-        center : [-74.5, 40],
-        zoom : 9 // starting zoom
-    });
+function addMap(id) {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYWNoZSIsImEiOiJja2hzMzNiZTAwZzhzMnFsaGRrb2RiazI1In0.Kas8OyDCtoysgjokizoasg';
+  map = new mapboxgl.Map({
+    container: "map",
+    //style: 'mapbox://styles/mapbox/satellite-v9', // stylesheet location
+    style: "mapbox://styles/mapbox/dark-v10",
+    center: [-74.5, 40],
+    zoom: 9 // starting zoom
+  });
 
-    map.on("load", function ()
-    {
-        mapCanvas = document.getElementsByClassName("mapboxgl-canvas")[0];
-    });
+  map.on("load", function () {
+    mapCanvas = document.getElementsByClassName("mapboxgl-canvas")[0];
+  });
 }
 
 
+
+
 var app = new Vue({
-    el : "#app",
-    delimiters: ['$[', ']$'],
-    data : {
-        message : "Hello Vue!",
-        messages : false,
-        cityinfo : false,
-        left : false,
-        push_str : "Hide",
-    },
-    mounted : function ()
-    {
-        addMap("map");
-    },
-    methods :
-        {
-            messagesShow : function ()
-            {
-                console.log("messages show");
-            },
-            messagesHide : function ()
-            {
-                console.log("messages hide");
-            },
-            cityShow : function ()
-            {
-                console.log("city show");
-            },
-            cityHide : function ()
-            {
-                console.log("city hide");
-            },
-            showHide : function ()
-            {
-                if (this.push_str === "Hide")
-                {
-                    this.push_str = "Show";
-                    this.left = false;
-                }
-                else
-                {
-                    this.push_str = "Hide";
-                    this.left = true;
-                }
-            },
-            cityInfo : function ()
-            {
-                console.log("city info");
-            },
-            getEvents : function ()
-            {
-                const xmlhttp = new XMLHttpRequest();
-                xmlhttp.open("GET", "/api/events");
-                xmlhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-                xmlhttp.setRequestHeader('Accept', 'application/json');
-                xmlhttp.onreadystatechange = function ()
-                {
-                    if (xmlhttp.readyState === 4 )
-                    {
-                        let data = JSON.parse(this.responseText);
+  el: "#app",
+  delimiters: ['$[', ']$'],
+  data: {
+    message: "Hello Vue!",
+    messages: false,
+    cityinfo: false,
+    left: false,
+    push_str: "Hide",
+    fireBtnStateStr: "Show",
+    fireBtnID: "",
+    earthBtnStateStr: "Show",
+    earthquakesID: ""
+  },
+  mounted: function () {
+    addMap("map");
+  },
+  methods:
+  {
+    showFire: function () {
+      if (this.fireBtnID === "")
+      {
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", "/api/events");
+        xmlhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+        xmlhttp.setRequestHeader('Accept', 'application/json');
+        xmlhttp.onreadystatechange = function () {
+          if (xmlhttp.readyState === 4) {
+            let data = JSON.parse(this.responseText);
 
-                    }
-                }
-                xmlhttp.send();
-            }
-
+          }
         }
+        xmlhttp.send();
+      }
+      else {
+
+      }
+
+    },
+    showEarth: function () {
+      if (this.earthquakesID === "") {
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", "/api/earth");
+        xmlhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+        xmlhttp.setRequestHeader('Accept', 'application/json');
+        xmlhttp.onreadystatechange = function () {
+          if (xmlhttp.readyState === 4) {
+            let data = JSON.parse(this.responseText);
+            this.earthquakesID = "earthquakes";
+            map.addSource(this.earthquakesID, {
+              "type": "geojson",
+              "data": data
+            });
+
+            map.addLayer({
+              'id': 'earthquakes-heat',
+              'type': 'heatmap',
+              'source': 'earthquakes',
+              'maxzoom': 9,
+              'paint': {
+                // Increase the heatmap weight based on frequency and property magnitude
+                'heatmap-weight': [
+                  'interpolate',
+                  ['linear'],
+                  ['get', 'mag'],
+                  0,
+                  0,
+                  6,
+                  1
+                ],
+                // Increase the heatmap color weight weight by zoom level
+                // heatmap-intensity is a multiplier on top of heatmap-weight
+                'heatmap-intensity': [
+                  'interpolate',
+                  ['linear'],
+                  ['zoom'],
+                  0,
+                  1,
+                  9,
+                  3
+                ],
+                // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+                // Begin color ramp at 0-stop with a 0-transparancy color
+                // to create a blur-like effect.
+                'heatmap-color': [
+                  'interpolate',
+                  ['linear'],
+                  ['heatmap-density'],
+                  0,
+                  'rgba(33,102,172,0)',
+                  0.2,
+                  'rgb(103,169,207)',
+                  0.4,
+                  'rgb(209,229,240)',
+                  0.6,
+                  'rgb(253,219,199)',
+                  0.8,
+                  'rgb(239,138,98)',
+                  1,
+                  'rgb(178,24,43)'
+                ],
+                // Adjust the heatmap radius by zoom level
+                'heatmap-radius': [
+                  'interpolate',
+                  ['linear'],
+                  ['zoom'],
+                  0,
+                  2,
+                  9,
+                  20
+                ],
+                // Transition from heatmap to circle layer by zoom level
+                'heatmap-opacity': [
+                  'interpolate',
+                  ['linear'],
+                  ['zoom'],
+                  7,
+                  1,
+                  9,
+                  0
+                ]
+              }
+            },
+              'waterway-label'
+            );
+
+            map.addLayer(
+              {
+                'id': 'earthquakes-point',
+                'type': 'circle',
+                'source': 'earthquakes',
+                'minzoom': 7,
+                'paint': {
+                  // Size circle radius by earthquake magnitude and zoom level
+                  'circle-radius': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    7,
+                    ['interpolate', ['linear'], ['get', 'mag'], 1, 1, 6, 4],
+                    16,
+                    ['interpolate', ['linear'], ['get', 'mag'], 1, 5, 6, 50]
+                  ],
+                  // Color circle by earthquake magnitude
+                  'circle-color': [
+                    'interpolate',
+                    ['linear'],
+                    ['get', 'mag'],
+                    1,
+                    'rgba(33,102,172,0)',
+                    2,
+                    'rgb(103,169,207)',
+                    3,
+                    'rgb(209,229,240)',
+                    4,
+                    'rgb(253,219,199)',
+                    5,
+                    'rgb(239,138,98)',
+                    6,
+                    'rgb(178,24,43)'
+                  ],
+                  'circle-stroke-color': 'white',
+                  'circle-stroke-width': 1,
+                  // Transition from heatmap to circle layer by zoom level
+                  'circle-opacity': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    7,
+                    0,
+                    8,
+                    1
+                  ]
+                }
+              },
+              'waterway-label'
+            );
+
+            this.earthBtnStateStr = "Hide";
+          }
+        }
+        xmlhttp.send();
+      }
+      else {
+          console.log("xd");
+            if (this.earthBtnStateStr === "Hide")
+            {
+                this.earthBtnStateStr = "Show";
+                map.setLayoutProperty(this.earthquakesID, 'visibility', 'none');
+            }
+            else
+            {
+                this.earthBtnStateStr = "Hide";
+                map.setLayoutProperty(this.earthquakesID, 'visibility', 'visible');
+            }
+      }
+
+    },
+    messagesShow: function () {
+      console.log("messages show");
+    },
+    messagesHide: function () {
+      console.log("messages hide");
+    },
+    cityShow: function () {
+      console.log("city show");
+    },
+    cityHide: function () {
+      console.log("city hide");
+    },
+    showHide: function () {
+      if (this.push_str === "Hide") {
+        this.push_str = "Show";
+        this.left = false;
+      }
+      else {
+        this.push_str = "Hide";
+        this.left = true;
+      }
+    },
+    cityInfo: function () {
+      console.log("city info");
+    },
+    getEvents: function () {
+
+    }
+
+  }
 })
