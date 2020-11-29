a3786dbc773c865b24dfc18586a02c9afca12ba6
Mark Turner <14060149+MarkTurner01@users.noreply.github.com>
2020-11-29 02:07:27 +0000

    Adding News

--- project_guh/api/urls.py
+++ project_guh/api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
         path('fire',FireView.as_view(), name='fire'),
         path('earth',EarthView.as_view(), name='earth'),
         path("events", EventsView.as_view(), name="events"),
-        path('news/<str:query>',NewsViewDirect.as_view(), name='test'),
-        path('newscached/<str:query>',NewsView.as_view(), name="test")
+        path('news',NewsViewDirect.as_view(), name='test'),
+        path('newscached',NewsView.as_view(), name="test")
 ]
